f9d79ed9b3abee94d39261ace8a225ac403705f8
Mukesh <mukeshfastapi.5050@gmail.com>
2025-09-06 13:54:01 +0530

    New Feature implemented

--- TO_DO_APP/web.py
+++ TO_DO_APP/web.py
@@ -3,6 +3,8 @@ import functions
 
 todos = functions.get_todos()
 
+st.set_page_config(layout="wide")
+
 def add_todo():
     new = st.session_state["new_todo"] + '\n'
     todos.append(new)
@@ -10,10 +12,19 @@ def add_todo():
 
 st.title ('My ToDo App')
 st.subheader("This is my ToDo App")
-st.write("This App will increase your productivity")
-
-for todo in todos:
-    st.checkbox(todo)
+st.write("<h2>This App will increase your <b>productivity</b></h2>",
+         unsafe_allow_html=True)
 
 st.text_input(label="", placeholder="Add new todo:",
               on_change=add_todo, key='new_todo')
+
+for index,todo in enumerate(todos):
+    check_box = st.checkbox(todo, key=todo)
+    if check_box:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[todo]
+        st.rerun()
+
+
+
